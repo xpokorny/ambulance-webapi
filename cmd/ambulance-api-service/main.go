dd260cae6b121c5cd90734859f6f95118b98bb96
Oliver Pokorný <opokorny@microstep-hdo.sk>
2025-05-09 21:50:52 +0200

    Kostra servera

--- cmd/ambulance-api-service/main.go
+++ cmd/ambulance-api-service/main.go
@@ -6,6 +6,7 @@ import (
     "strings"
     "github.com/gin-gonic/gin"
     "github.com/xpokorny/ambulance-webapi/api"
+	"github.com/xpokorny/ambulance-webapi/internal/ambulance_wl"
 )
 
 func main() {
@@ -21,6 +22,11 @@ func main() {
     engine := gin.New()
     engine.Use(gin.Recovery())
     // request routings
+	handleFunctions := &ambulance_wl.ApiHandleFunctions{
+		AmbulanceConditionsAPI:  ambulance_wl.NewAmbulanceConditionsApi(),
+		AmbulanceWaitingListAPI: ambulance_wl.NewAmbulanceWaitingListApi(),
+	}
+	ambulance_wl.NewRouterWithGinEngine(engine, *handleFunctions)
     engine.GET("/openapi", api.HandleOpenApi)
     engine.Run(":" + port)
 }
